ea9c6f6bc0110e2f8449f016ccfcb130275db604
Walter Dundore <walter.dundore@uga.edu>
2023-11-10 08:27:09 -0500

    testing

---
+++ Puppetfile
@@ -0,0 +1,13 @@
+forge "https://forge.puppet.com"
+
+mod 'puppetlabs-stdlib', '9.3.0'
+mod 'saz/sudo', '8.0.0'
+mod 'puppet-selinux', '4.0.0'
+mod 'puppetlabs-docker', '9.1.0'
+mod 'puppet-epel', '5.0.0'
+
+mod 'puppet-code',
+     :git    => 'git@github.com:waltdundore/k12sysadmin-control-repo.git',
+     :branch => :control_branch,
+     :default_branch => 'production'
+
---
+++ manifests/site.pp
@@ -0,0 +1,63 @@
+node default {
+#  include ::roles::standalone
+}
+
+package { 'git':
+  ensure => installed,
+}
+
+package { 'tmux':
+  ensure => installed,
+}
+
+
+###############
+# sudo config #
+###############
+include sudo
+
+sudo::conf { 'puppet_puppet':
+    content  => 'puppet ALL=NOPASSWD: /usr/bin/puppet, /usr/local/bin/puppet',
+  }
+sudo::conf { 'vagrant':
+  content => "Defaults:vagrant !requiretty\nvagrant ALL=(ALL) NOPASSWD: ALL",
+}
+
+#################
+# docker config #
+#################
+#class { 'docker':
+#  docker_users => ['wdundore'],
+#}
+
+
+
+# default path for commands
+#Exec { path => '/usr/bin:/bin:/usr/sbin:/sbin' }
+
+# by default all files overwritten by puppet
+# will be backed up on the puppetmaster
+#filebucket { 'main':  server => 'puppet.apidb.org' }
+#File { backup => main }
+
+# default permissions of 644 for files and 755 for directories
+File { mode => '0644' }
+
+# files default to being owned by root
+File {
+  owner => root,
+  group => root,
+}
+
+# ignore version control when recursively managing directories
+File { ignore => ['.svn', '.git', 'CVS' ] }
+
+# default options for mounts
+Mount {
+  dump => 0,
+  pass => 0,
+}
+
+Package {
+  allow_virtual => true,
+}
